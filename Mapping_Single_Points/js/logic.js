5ac88357505253477a41db17fa666254db5803a6
Soco Hernandez <85274353+chocoplace@users.noreply.github.com>
2021-09-12 20:19:15 -0400

    Add files via upload

--- Mapping_Single_Points/js/logic.js
+++ Mapping_Single_Points/js/logic.js
@@ -13,14 +13,22 @@ var map = L.map('mapid').setView([40.7, -94.5], 4);
    //zoom: 4
 //})
 
+// Add a marker to the map for Los Angeles, California.
+//let marker = L.marker([34.0522, -118.2437]).addTo(map);
+L.circleMarker([34.0522, -118.2437], {
+  radius: 300,
+  color: "black",
+  fillColor: '#ffffa1'
+}).addTo(map);
+
 // We create the title layer that will be the background of our map
-let streets = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/streets-v11/tiles/{z}/{x}/{y}?access_token={accessToken}', {
+let streets = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/dark-v10/tiles/{z}/{x}/{y}?access_token={accessToken}', {
     attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
     maxZoom: 18,
-    id: 'mapbox/streets-v11',
+    id: 'dark-v10',
     tileSize: 512,
     zoomOffset: -1,
     accessToken: API_KEY
 });
 //Then we add our 'graymap' title layer to the map
-streets.addTo(map);
+streets.addTo(map)
